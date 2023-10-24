21aff6a5da428ac92db8551722a9e2dfb4cf7d24
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-10-24 19:22:31 +0000

    Construção JavaScript

--- briefing.js
+++ briefing.js
@@ -1,6 +1,6 @@
-const motivacional = [
-  {tag1:' A vida é uma sequência de pensamentos.'},
-  {tag2:' A coragem está um passo a frente do medo.'},
-  {tag3:' Se você não puder destacar-se pelo talento, vença pelo esforço.'},
-  {tag4: ' Acredite em você'},
-  {tag5: ' Quando a persistência e o foco se unem, os sonhos se sealizam.'}]
+const motivation = [
+  {tag:' A vida é uma sequência de pensamentos.'},
+  {tag:' A coragem está um passo a frente do medo.'},
+  {tag:' Se você não puder destacar-se pelo talento, vença pelo esforço.'},
+  {tag: ' Acredite em você'},
+  {tag: ' Quando a persistência e o foco se unem, os sonhos se sealizam.'}]
--- script.js
+++ script.js
@@ -17,42 +17,37 @@ function typeWrite(elemento){
 const me = document.querySelector("#me")
 const takes = document.querySelector("#takes")
 const more = document.querySelector("#more")
+let index = 0
+
+                                
 
 
 
 
 
+window.onload = message()
 
 
-window.onload = motivation()
 
-function motivation (){
-
-let phrases 
-let take = 0
-  
-phrases = setInterval(function () {
-
- 
-  takes.innerHTML = take
-  }, 6000)
-
+function message(){
+  if (index < motivation.lenght) {
+    takes.innerHTML = motivation[index]
+    index++;
+  } else {
+    clearInterval(interval)
+  }
 }
 
+let interval = setInterval(message, 500)
 
-
+console.log(setInterval)
 
 function aboutMe() {
 
-  me.innerHTML = ('Nascido em 1992, e raizes fundadas no interior de São Paulo/Piracicaba, eis que nasce um garoto sonhador que acredita que se não desistir pode alcançar o que  almeja, nessa longa jornada apresento á vocês um pouco do trabalho e projetos alcançados pelas garras da determinação de alcançar seu destino.')
+ me.innerHTML = ('Nascido em 1992, e raizes fundadas no interior de Piracicaba/SP, lhe apresento um garoto sonhador que acredita que se não desistir pode alcançar o que  almeja, atualmente entre estudos e trabalho, nessa longa jornada apresento á vocês um pouco das criações e projetos alcançados pelas garras da determinação de alcançar seu destino.')
   
   more.innerHTML =('Ver mais...')
 
   more.style.padding = "2px"
-
-
-console.log(motivacional)
   
 }
-
-
