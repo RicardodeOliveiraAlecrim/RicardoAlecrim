68e98212f5fe55aa04bed5d805a369cfc2a0bf5a
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-10-24 08:04:03 +0000

    Adição script

---
+++ script.js
@@ -0,0 +1,25 @@
+const about = document.querySelector("#about-it")
+const me = document.querySelector("#me")
+
+
+window.onload = motivation()
+
+function motivation (){
+
+let phrases = 
+
+phrases = setInterval(function () {
+  me.innerHTML = ('oi')
+  }, 3600000)
+
+}
+
+function aboutMe() {
+
+  me.innerHTML = ('Nascido em 1992, e raizes fundadas no interior de São Paulo/Piracicaba, eis que nasce um garoto sonhador que acredita que se não desistir pode alcançar o que  almeja, nessa longa jornada apresento á vocês um pouco do trabalho e projetos alcançados pelas garras da determinação de alcançar seu destino.')
+  
+console.log(motivacional)
+  
+}
+
+
