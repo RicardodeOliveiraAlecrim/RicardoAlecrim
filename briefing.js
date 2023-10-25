44598ce0fc7772f4670cd87ff8cfd592cd5a7d3c
RicardodeOliveiraAlecrim <ricardoalecrim2015@gmail.com>
2023-10-24 21:18:29 -0300

    Exclusão botão construção JavaScript

--- briefing.js
+++ briefing.js
@@ -1,6 +1,6 @@
 const motivation = [
-  {tag:' A vida é uma sequência de pensamentos.'},
-  {tag:' A coragem está um passo a frente do medo.'},
-  {tag:' Se você não puder destacar-se pelo talento, vença pelo esforço.'},
-  {tag: ' Acredite em você'},
-  {tag: ' Quando a persistência e o foco se unem, os sonhos se sealizam.'}]
+  " A vida é uma sequência de pensamentos.",
+  " A coragem está um passo a frente do medo.",
+   "Se você não puder destacar-se pelo talento, vença pelo esforço.",
+   " Acredite em você",
+   " Quando a persistência e o foco se unem, os sonhos se sealizam."];
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function typeWrite(elemento){
+function typeWrite(elemento) {
 
   const textoArray = elemento.innerHTML.split('');
+
   elemento.innerHTML = ' ';
-  textoArray.forEach(function(letra, i){   
 
-  setTimeout(function(){
+  textoArray.forEach(function (letra, i) {
+
+    setTimeout(function () {
+
       elemento.innerHTML += letra;
-  }, 75 * i)
+    }, 75 * i)
 
-});
+  });
 }
 
 
@@ -17,37 +20,28 @@ function typeWrite(elemento){
 const me = document.querySelector("#me")
 const takes = document.querySelector("#takes")
 const more = document.querySelector("#more")
-let index = 0
-
-                                
 
 
+let index = 0;
 
-
-
-window.onload = message()
-
-
-
-function message(){
+function nextMessage() {
   if (index < motivation.lenght) {
-    takes.innerHTML = motivation[index]
+    takes.innerHTML = motivation[index];
     index++;
-  } else {
-    clearInterval(interval)
+    setTimeout(nextMessage, 6000);
   }
 }
 
-let interval = setInterval(message, 500)
 
-console.log(setInterval)
+nextMessage()
+
+
 
 function aboutMe() {
 
- me.innerHTML = ('Nascido em 1992, e raizes fundadas no interior de Piracicaba/SP, lhe apresento um garoto sonhador que acredita que se não desistir pode alcançar o que  almeja, atualmente entre estudos e trabalho, nessa longa jornada apresento á vocês um pouco das criações e projetos alcançados pelas garras da determinação de alcançar seu destino.')
-  
-  more.innerHTML =('Ver mais...')
+  me.style.opacity = 1
 
-  more.style.padding = "2px"
+  typeWrite(me)
   
 }
+
